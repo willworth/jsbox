310433b04002f776b2c012fb878a89441866f4b9
Will <code@willworth.co.uk>
2016-09-22 05:20:07 +0200

    initial

---
+++ js.js
@@ -0,0 +1,12 @@
+
+console.log("Hello");
+console.log(5 + 969);
+window.alert(5 + 6);
+
+
+document.write(5 + 4);
+console.log("Hello");
+
+
+http://www.thewindowsclub.com/numlock-startup-windows-7-8
+2147483648
---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+function popup() {
+alert("This is the alert text from the popup function")
+}
+
+
+// This is a single line JavaScript comment.
+
+
+
+/*document.write("You can't see this!");
+document.write("You can't see this!");
+document.write("You can't see this!");
+document.write("You can't see this!");
+document.write("You can't see this!");
+document.write("You can't see this!");
+document.write("You can't see this!");*/
