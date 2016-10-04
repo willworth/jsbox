6d238cfb12165424c587682d39453903fbe42137
Will <code@willworth.co.uk>
2016-10-04 23:48:52 +0200

    taxes...

--- js.js
+++ js.js
@@ -1,7 +1,36 @@
-function titleCase(str) {
- var amy = str.split(" ");
-  amy = str.toLowerCase();
-   return amy;
-}
+const TAX_RATE  = 1.23;
 
-titleCase("I'm a little tea pot");
+var price = prompt ("how much does the phone cost?")
+
+console.log("You have entered " + price);
+
+var taxed = price * TAX_RATE;
+taxed = taxed.toFixed(2);  //rounds to 2 decimal places
+console.log("the total, with a tax rate of ", + TAX_RATE,  " is " + taxed );
+
+
+
+
+
+, with a tax rate of ", + TAX_RATE, + " 
+
+
+
+
+
+
+with a tax rate of ", + TAX_RATE, + "
+
+
+
+
+
+
+
+// function titleCase(str) {
+//  var amy = str.split(" ");
+//   amy = str.toLowerCase();
+//    return amy;
+// }
+
+// titleCase("I'm a little tea pot");
