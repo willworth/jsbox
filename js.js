807977fb97e9accb82d57f26ab1e89b6bca37bf0
Will <code@willworth.co.uk>
2016-10-05 00:05:29 +0200

    update loop for phone purchases

--- js.js
+++ js.js
@@ -1,25 +1,26 @@
 const TAX_RATE  = 1.23;
-
+var bankBalance = prompt("How much money do you have in your bank acount?");
+var fundsRemaining = prompt("How much would you like to have left\
+after your spending spree?");
 var price = prompt ("how much does the phone cost?")
 
 console.log("You have entered " + price);
 
+
+var numberOfPhones = 0;
 var taxed = price * TAX_RATE;
 taxed = taxed.toFixed(2);  //rounds to 2 decimal places
 console.log("the total, with a tax rate of ", + TAX_RATE,  " is " + taxed );
 
+while (bankBalance > fundsRemaining){
+    bankBalance -= taxed;
+    numberOfPhones +=1;
+    console.log("Price is €" + taxed);
 
-
-
-
-, with a tax rate of ", + TAX_RATE, + " 
-
-
-
-
-
-
-with a tax rate of ", + TAX_RATE, + "
+}
+bankBalance =bankBalance.toFixed(2);
+console.log ("we eneded up with a balance of" +
+bankBalance + "and " + numberOfPhones + "new phones!")
 
 
 
