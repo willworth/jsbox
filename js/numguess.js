13f813b846177a246c2343d285d5a6974928d483
will <code@willworth.co.uk>
2016-11-15 15:53:00 +0100

    build basic js

--- js/numguess.js
+++ js/numguess.js
@@ -1,3 +1,53 @@
+
+var guessCount = 0;
+
+
 function popup() {
-alert("This is numguess js!")
+alert(guessCount);
+ }
+
+function popup() {
+alert("Guess a number between 1 -100!")
 }
+
+
+
+
+
+
+
+function getNum (){
+//generate answer
+var randomNumber = Math.random() * 100;  //generates between 0- 99.99...
+var numFloored = Math.floor(randomNumber);//floors the num
+var goalNum = numFloored + 1; // this way it's 1-100 not 0-99
+
+
+
+    var guess = prompt("Tell me your number")
+
+}
+
+
+
+
+
+
+
+
+// popup describe game, then requestnum
+
+// check num ok
+
+// put num in var
+
+// gen random num
+
+// if num correct
+// print correct
+// else
+// if high
+// print too high and offer box to guess again
+
+// if low 
+// print too low and offer box to guess again
