0cec793df610c33a53a32571cd5aad85a5fc305a
will <code@willworth.co.uk>
2016-11-18 14:00:54 +0100

    working guess game

--- js/numguess.js
+++ js/numguess.js
@@ -3,43 +3,24 @@ var guessCount = 0;
 var guessText;
 var guess;
 var finished = false;
+var goalNum;
 
 function startGame(){
     var randomNumber = Math.random() * 100;  //generates between 0- 99.99...
     var numFloored = Math.floor(randomNumber);//floors the num
-    var goalNum = numFloored + 1; // this way it's 1-100 not 0-99
+     goalNum = numFloored + 1; // this way it's 1-100 not 0-99
 
     while (!finished){
-
-
-
-
     guessText = prompt("Give me a number" +
                         "in the range of 1-100.\n\n" +
                         "What is the number?");
     guess = parseInt(guessText);
     guessCount +=1;
-    alert("You guessed "+ guess);
-
+    
+    finished = checkNumber(); 
 
     }
 }   
-
-
-
-
-// function startGame(){
-//     alert("Here we go!", "other")
-// }   
-
-
-// function popup() {
-// alert(guessCount);
-//  }
-
-// function popup() {
-// alert("Guess a number between 1 -100!");
-// }
 
 
 
@@ -55,7 +36,19 @@ function checkNumber() {
         alert("Please enter a number between 1-100!");
         return false
     }
-
+    if (guess > goalNum){
+        alert("Your number is too high!");
+        return false;
+    }
+    if (guess < goalNum){
+        alert("Your number is too low!");
+        return false;
+    }
+    alert("You got it! The number was " +
+    goalNum +"\n \n It took  you " + guessCount +
+    " guesses."
+    )
+    return true;
 }
 
 
